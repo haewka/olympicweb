ae26ea64a5f0577c79b7c0ffade18f518b453ca6
haewka <nameitsfanny@mail.ru>
2021-07-30 14:32:29 +0300

    add adaptive

--- src/main/js/script.js
+++ src/main/js/script.js
@@ -1,16 +1,32 @@
 $(document).ready(function(){
-    $('.performance__slider').slick({
+    $('.performance__slider,.performance__secondslider').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="img/left.png" alt="slide1" class="left-slick"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/right.png" alt="slide1" class="right-slick"></button>',
-        speed: 1000,
-        fade: true
+        speed: 600,
+        fade: true,
+        infinite: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                arrows: false,
+                dots: true
+              }
+            }
+        ]
     });
   });
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.performance__secondslider').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="img/leftblack.png" alt="slide1" class="left-slick"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/rightblack.png" alt="slide1" class="right-slick"></button>',
-        speed: 1000,
+        speed: 600,
         fade: true
     });
-});
+}); */
+
+/* $(document).ready(function(){
+  $('').slick({
+
+  });
+}); */
